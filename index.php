7e4e936f9b43f0fca9f08438774b1c7e0fad2691
Andres Jimenez <amaujipe@gmail.com>
2019-01-03 21:47:01 -0500

    Se creo la pagina de inicio

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1>Ejercicio de PHP</h1>";
+echo "<ul>";
+  echo "<li><a href='ejercicio-01'><strong>Ejercicio 1: </strong></a>Hacer un programa en PHP que tenga un array con 8 numeros enteros y que haga lo siguiente:";
+    echo "<ul>";
+      echo "<li>Recorrer el arreglo y mostrarlo</li>";
+      echo "<li>Ordenarlo y mostrarlo</li>";
+      echo "<li>Mostrar su longitud</li>";
+      echo "<li>Buscar algun elemento</li>";
+    echo "</ul>";
+  echo "</li>";
+  echo "<hr>";
+echo "</ul>";
